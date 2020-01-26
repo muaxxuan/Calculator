f61089f1022c2c421681b2f269510848cbabfc72
muaxxuan <49665292+muaxxuan@users.noreply.github.com>
2020-01-25 20:14:42 -0500

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,32 @@
+import re
+
+print("Welcome to Xuan's first Calculator!")
+print("Type 'quit' to exit\n")
+
+previous = 0
+run = True
+
+
+def perform_math():
+    global run
+    global previous
+    equation = ""
+    if previous == 0:
+        equation = input("Enter equation:")
+    else:
+        equation = input(str(previous))
+
+    if equation == 'quit':
+        print("Goodbye!")
+        run = False
+    else:
+        equation = re.sub('[a-zA-Z,.:()" "]', '', equation)
+
+        if previous == 0:
+            previous = eval(equation)
+        else:
+            previous = eval(str(previous) + equation)
+
+
+while run:
+    perform_math()
